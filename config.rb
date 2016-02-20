616cbdc079e207e1d5c6a204a41d1f7079b35c16
Caro Byoute <caroline.beyne@gmail.com>
2016-02-20 11:32:26 +0100

    config.rb change for compass compile in correct folders

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path       = "/"
-css_dir         = "assets/css/"
-images_dir      = "assets/images/"
-javascripts_dir = "assets/js/"
+css_dir         = "source/assets/css/"
+images_dir      = "source/assets/images/"
+javascripts_dir = "source/assets/js/"
 sass_dir        = "_assets/sass/"
 
 output_style    = :nested #:expanded or :nested or :compact or :compressed
